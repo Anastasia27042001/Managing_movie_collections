e21c0164ce088bd2caddbf03383424090b44d93d
Anastasia27042001 <obukhova.nastya2017@yandex.ru>
2025-05-21 14:53:40 +0300

    написание методов и итератора

--- movie_collection.py
+++ movie_collection.py
@@ -1,23 +1,67 @@
 class Movie:
-    def __init__(self, id: int, title: str, genre: str, year: int):
-        self.id = id
+    def __init__(self, title: str, genre: str, year: int):
         self.title = title
         self.genre = genre
         self.year = year
 
+    def __repr__(self):
+        return f'Фильм {self.title}, жанр {self.genre}, {self.year} года'
+
 class MovieCollection:
     def __init__(self):
-        self.movies = {}
+        self.movies: dict[str, Movie] = {}
 
     def add_movie(self, movie: Movie) -> bool:
-        if movie not in self.movies:
+        if movie.title in self.movies:
+            print(f'Фильм {movie.title} уже существует')
+            return False
+        else:
+            self.movies[movie.title] = movie
+            print(f'Фильм {movie.title} добавлен')
+            return True
 
-    def remove_movie(self, movie: Movie) -> bool:
+    def remove_movie(self, title: str) -> bool:
+        if title not in self.movies:
+            print('Фильм не найден')
+            return False
+        else:
+            del self.movies[title]
+            print(f'Фильм {title} удален')
+            return True
 
-    def find_movie(self, title: str) -> Movie:
+    def find_movie_title(self, title: str) -> list[Movie]:
+        result = [i for i in self.movies.values() if title in i.title]
+        print(result)
+        return result
 
-movie_collection = MovieCollection()
+    def find_movie_genre(self, genre: str) -> list[Movie]:
+        result = [i for i in self.movies.values() if genre in i.genre]
+        print(result)
+        return result
+
+    def find_movie_year(self, year: int) -> list[Movie]:
+        result = [i for i in self.movies.values() if year == i.year]
+        print(result)
+        return result
+
+    def __iter__(self) -> 'MovieIterator':
+        return MovieIterator(list(self.movies.values()))
+
 
 class MovieIterator:
-    def __init__(self, collection: movie_collection):
-        self.collection = collection
+    def __init__(self, movies):
+        self._movies = list(movies)
+        self._index = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self._index < len(self._movies):
+            movie = self._movies[self._index]
+            self._index += 1
+            return movie
+        else:
+            raise StopIteration
+
+collection = MovieCollection()
