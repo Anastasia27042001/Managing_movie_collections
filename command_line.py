4ef8fdeccbda9323570acdbcf94f78ff174898d4
Anastasia27042001 <obukhova.nastya2017@yandex.ru>
2025-05-21 15:06:17 +0300

    добавлены команды, вызывающие методы классов

---
+++ command_line.py
@@ -0,0 +1,51 @@
+from movie_collection import *
+
+def user_commands():
+    while True:
+        command = input('Введите свою команду - добавление/удаление/поиск/список/завершение: ').lower()
+
+        if command == 'добавление':
+            title = input('Введите название фильма: ')
+            genre = input('Введите жанр фильма: ')
+            year = int(input('Введите год выпуска фильма: '))
+
+            new_movie = Movie(title=title, genre=genre, year=year)
+            collection.add_movie(new_movie)
+
+
+        elif command == 'удаление':
+            title = input('Введите название фильма: ')
+            collection.remove_movie(title)
+
+        elif command == 'поиск':
+            criterion = input('Введите критерий поиска - название/жанр/год: ').lower()
+
+            if criterion == 'название':
+                value = input('Введите название: ').lower()
+                print(collection.find_movie_genre(value))
+
+            elif criterion == 'жанр':
+                value = input('Введите жанр: ').lower()
+                print(collection.find_movie_genre(value))
+
+            elif criterion == 'год':
+                value = int(input('Введите год: '))
+                print(collection.find_movie_genre(value))
+
+            else:
+                print('Критерий поиска не распознан')
+
+        elif command == 'список':
+
+            print('\nСписок фильмов:')
+            for i, movie in enumerate(collection, 1):
+                print(f"{i}. {movie}")
+
+        elif command == 'завершение':
+            print('Всего доброго!')
+            break
+
+        else:
+            print('Команда не распознана')
+
+user_commands()
