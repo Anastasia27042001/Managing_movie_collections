2389f142d3877506d9152b64d2bd91b73726f39c
Anastasia27042001 <obukhova.nastya2017@yandex.ru>
2025-06-04 13:39:50 +0300

    доработка тестов

--- test_methods.py
+++ test_methods.py
@@ -2,11 +2,6 @@ import pytest
 from movie_collection import *
 from test_fixtures import *
 
-@pytest.fixture
-def collection():
-    return MovieCollection()
-
-
 def test_add_valid_movie(collection, generate_valid_movie):
     movie_data = generate_valid_movie
     movie = Movie(**movie_data)
@@ -16,10 +11,12 @@ def test_add_valid_movie(collection, generate_valid_movie):
     assert movie.title in collection.movies
 
 
-def test_add_invalid_movie(collection, generate_invalid_movie):
-    movie_data = generate_invalid_movie
+def test_creation_invalid_movie(collection, generate_invalid_movie):
     with pytest.raises(ValueError):
+        movie_data = generate_invalid_movie
+        print('Некорректно введены данные')
         Movie(**movie_data)
+
 
 
 def test_add_duplicate_movie(collection, generate_valid_movie):
@@ -34,7 +31,7 @@ def test_add_duplicate_movie(collection, generate_valid_movie):
     assert len(collection.movies) == 1
 
 
-def test_remove_existing_movie(collection, generate_valid_movie):
+def test_remove_movie(collection, generate_valid_movie):
     movie_data = generate_valid_movie
     movie = Movie(**movie_data)
     collection.add_movie(movie)
@@ -49,8 +46,10 @@ def test_find_by_title(collection, generate_valid_movie):
     movie = Movie(**movie_data)
     collection.add_movie(movie)
 
-    results = collection.find_by_title(movie.title[:3])
-    assert len(results) >= 1
+    search_query = movie.title.split()[0].lower()
+    results = collection.find_movie_title(search_query)
+    print('Результат поиска:', results)
+
     assert movie in results
 
 
@@ -59,7 +58,8 @@ def test_find_by_genre(collection, generate_valid_movie):
     movie = Movie(**movie_data)
     collection.add_movie(movie)
 
-    results = collection.find_by_genre(movie.genre)
+    results = collection.find_movie_genre(movie.genre)
+    print('Результат поиска:', results)
     assert all(m.genre == movie.genre for m in results)
 
 
@@ -68,5 +68,7 @@ def test_find_by_year(collection, generate_valid_movie):
     movie = Movie(**movie_data)
     collection.add_movie(movie)
 
-    results = collection.find_by_year(movie.year)
+    results = collection.find_movie_year(movie.year)
+    print('Результат поиска:', results)
     assert all(m.year == movie.year for m in results)
+
